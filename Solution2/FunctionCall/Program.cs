67719ea50047fc3f608c197ead30ad9243b3ea6a
Alif <imalif.shaikh999@gmail.com>
2024-01-29 21:55:42 +0530

    function calling through object

---
+++ Solution2/FunctionCall/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionCall
+{
+    class Demo
+    {
+        public void Alif()
+        {
+            Console.WriteLine("hiii...Im Alif");
+        }
+        internal void Amol()
+        {
+            Console.WriteLine("hiii...Im Amol");
+        }
+        private void Aniket()
+        {
+            Console.WriteLine("hiii...Im Aniket");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo obj=new Demo();
+            obj.Alif();
+            obj.Amol();
+
+
+        }
+    }
+}
