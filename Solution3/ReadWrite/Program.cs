9fc8d0bc36fc7dd8444cff7df1cb663fdcf71cc0
Alif <imalif.shaikh999@gmail.com>
2024-01-29 22:35:16 +0530

    taking inputs from the user

---
+++ Solution3/ReadWrite/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter Your Name...");
+            
+            name= Console.ReadLine();
+
+            //  Console.WriteLine("My name is " + name);
+            Console.WriteLine("My name is {0} ",name);
+        }
+    }
+}
