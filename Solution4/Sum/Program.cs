2360520ad942fc22f6ec033314b5b80e300de21b
Alif <imalif.shaikh999@gmail.com>
2024-01-30 16:10:45 +0530

    adding two numbers

---
+++ Solution4/Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+   
+    internal class Program
+    {
+        public void Add(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Sum of {0} and {1} is {2}", a, b, c);
+        }
+        static void Main(string[] args)
+        {
+
+            int a, b;
+            Console.WriteLine("Enter Two Numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Program s = new Program();
+            s.Add(a, b);
+        }
+    
+    }
+}
