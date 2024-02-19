4d56e09ed8be948aee20a24f75edae8d03e4e626
Alif <imalif.shaikh999@gmail.com>
2024-02-19 15:02:34 +0530

    conditionalloops

---
+++ Solution5/ForLoop/Factors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Factors
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            Console.WriteLine("factors of 20 are ...");
+            for (int i = 1; i <= n; i++)
+            {
+                if(n%i==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
